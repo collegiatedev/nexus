84e23de1cc5f8e1be28df1f829bcd1a66cc66345
Jesse Lee <jesselee@Jesses-MacBook-Air.local>
2024-05-17 14:58:37 -0700

    added supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
-
+import { defineConfig } from "drizzle-kit";
 import { env } from "~/env";
 
-export default {
+export default defineConfig({
   schema: "./src/server/db/schema.ts",
+  out: "./supabase/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
   tablesFilter: ["nexus_*"],
-} satisfies Config;
+});
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,15 +4,22 @@ import postgres from "postgres";
 import { env } from "~/env";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+// double check if im using transaction pool
+// Disable prefetch as it is not supported for "Transaction" pool mode
+// const client = postgres(env.DATABASE_URL, { prepare: false })
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const client = postgres(env.DATABASE_URL);
+export const db = drizzle(client, { schema });
 
-export const db = drizzle(conn, { schema });
+// /**
+//  * Cache the database connection in development. This avoids creating a new connection on every HMR
+//  * update.
+//  */
+// const globalForDb = globalThis as unknown as {
+//   conn: postgres.Sql | undefined;
+// };
+
+// const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+// if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+
+// export const db = drizzle(conn, { schema });
