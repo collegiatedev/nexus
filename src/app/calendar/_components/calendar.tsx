ffcf6ca6974c9f2f36acde2245941ef9137123d8
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-05 00:33:30 -0400

    scrolling basics working

--- src/app/calendar/_components/calendar.tsx
+++ src/app/calendar/_components/calendar.tsx
@@ -1,12 +1,30 @@
+"use client";
+
+import { useInView } from "react-intersection-observer";
 import { DnDBoard } from "./dnd/board";
 import { Month } from "./month";
+import { useEffect } from "react";
+import React from "react";
 
 export const Calendar = () => {
+  const [scrollTrigger, isInView] = useInView();
+  const months = [5];
+
+  useEffect(() => {
+    if (isInView) {
+      // loadMoreMonths();
+      console.log("scrolled");
+    }
+  }, [isInView]);
   return (
     <DnDBoard>
-      <Month whichMonth={4} />
-      <div className="h-[100px]" />
-      <Month whichMonth={5} />
+      <Month />
+      {months.map((month, idx) => (
+        <React.Fragment key={idx}>
+          <Month whichMonth={month} />
+          <div ref={scrollTrigger} className="relative bottom-[50vh]" />
+        </React.Fragment>
+      ))}
     </DnDBoard>
   );
 };
