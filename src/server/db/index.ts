f51436f381436424af59ff8771003184d920479b
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-07 18:20:13 -0400

    migrated queries

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -12,6 +12,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db: ReturnType<typeof createPrismaClient> =
+  globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,55 +1,70 @@
-// import "server-only";
+import "server-only";
 
-// import { db } from "./db";
-// import { userAuth } from "./wrapper";
-// import { eq } from "drizzle-orm";
-// import { InsertTask, tasks, taskTags } from "./db/schema";
+import { db } from "./db";
+import { userAuth } from "./wrapper";
+import { type Prisma } from "@prisma/client";
 
-// // todo: add request pagation and month filter
-// export const getMyTasks = userAuth(async (authContext) => {
-//   const { userId } = authContext;
+// todo: add request pagation and month filter
+export const getMyTasks = userAuth(async (authContext) => {
+  const { userId } = authContext;
 
-//   return await db.query.tasks.findMany({
-//     orderBy: (model, { asc }) => asc(model.due_date),
-//     // where: (model, { eq }) => eq(model.userId, userId),
-//     with: {
-//       tags: true,
-//     },
-//   });
-// });
-// export type MyTasks = Awaited<ReturnType<typeof getMyTasks>>;
+  return await db.task.findMany({
+    orderBy: {
+      dueDate: "asc",
+    },
+    // where: {
+    //   userId: userId,
+    // },
+    include: {
+      taskTags: true,
+    },
+  });
+});
+export type MyTasks = Awaited<ReturnType<typeof getMyTasks>>;
 
-// export const getMyTask = userAuth(async (authContext, imgId: number) => {
-//   const { userId } = authContext;
-//   const task = await db.query.tasks.findFirst({
-//     where: (model, { eq }) => eq(model.id, imgId),
-//     with: {
-//       tags: true,
-//     },
-//   });
+export const getMyTask = userAuth(async (authContext, imgId: number) => {
+  const { userId } = authContext;
+  const task = await db.task.findFirst({
+    where: {
+      id: imgId,
+    },
+    include: {
+      taskTags: true,
+    },
+  });
 
-//   if (!task) throw new Error("Image not found");
-//   // if (task.userId !== userId) throw new Error("Unauthorized");
+  if (!task) throw new Error("Image not found");
+  // if (task.userId !== userId) throw new Error("Unauthorized");
 
-//   return task;
-// });
+  return task;
+});
 
-// export const updateTaskDueDate = userAuth(
-//   async (_authContext, taskId: number, newDueDate: Date) => {
-//     await db
-//       .update(tasks)
-//       .set({ due_date: newDueDate.toDateString() })
-//       .where(eq(tasks.id, taskId));
-//   },
-// );
+export const updateTaskDueDate = userAuth(
+  async (_authContext, taskId: number, newDueDate: Date) => {
+    await db.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        dueDate: newDueDate,
+      },
+    });
+  },
+);
 
-// export const createTask = userAuth(
-//   async (_authContext, _res, task: InsertTask) => {
-//     await db.insert(tasks).values(task);
-//   },
-// );
+export const createTask = userAuth(
+  async (_authContext, _res, task: Prisma.TaskCreateInput) => {
+    await db.task.create({
+      data: task,
+    });
+  },
+);
 
-// export const deleteMyTaskTag = userAuth(async (authContext, tagId: number) => {
-//   const { userId } = authContext;
-//   await db.delete(taskTags).where(eq(taskTags.id, tagId));
-// });
+export const deleteMyTaskTag = userAuth(async (authContext, tagId: number) => {
+  const { userId } = authContext;
+  //   await db.taskTag.delete({
+  //     where: {
+  //       id: tagId,
+  //     },
+  //   });
+});
