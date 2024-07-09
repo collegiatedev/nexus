b037fe668b0b6f8ad8e39411751a7b97140c570d
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-09 14:44:08 -0400

    webhook setup

--- src/app/calendar/task/[id]/_components/fields/assignedField.tsx
+++ src/app/calendar/task/[id]/_components/fields/assignedField.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { addMinutes } from "date-fns";
 import { TaskFields, FieldFormat } from "./fieldsTable";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+import { useUser } from "@clerk/nextjs";
 
 export const AssignedField = ({ task }: TaskFields) => {
   return (
@@ -11,13 +15,19 @@ export const AssignedField = ({ task }: TaskFields) => {
 
 const AssignedUser = () => {
   // get user using api call
+  const { user } = useUser();
+
+  const memberships = user?.organizationMemberships;
+  //   memberships?.name
+
   return (
     <div className="flex cursor-pointer items-center gap-2">
       <Avatar className="h-8 w-8">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarImage src={user?.imageUrl} />
+        <AvatarFallback>C</AvatarFallback>
       </Avatar>
-      john@doe.com
+      <p className="font-medium">{user?.fullName} -</p>
+      {memberships?.map((m) => m.organization.name)}
     </div>
   );
 };
