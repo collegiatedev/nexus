a7cdca55a7a9ed3d1899a1786592c53c955d04ff
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-09 18:35:10 -0400

    skeleton for assignedUser

--- src/app/calendar/task/[id]/_components/fields/assignedField.tsx
+++ src/app/calendar/task/[id]/_components/fields/assignedField.tsx
@@ -3,6 +3,8 @@
 import { TaskFields, FieldFormat } from "./fieldsTable";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { useUser } from "@clerk/nextjs";
+import { Skeleton } from "~/components/ui/skeleton";
+import { useState, useEffect } from "react";
 
 export const AssignedField = ({ task }: TaskFields) => {
   return (
@@ -15,17 +17,31 @@ export const AssignedField = ({ task }: TaskFields) => {
 const AssignedUser = () => {
   // get user using api call
   const { user } = useUser();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      setIsLoading(false);
+    }
+  }, [user]);
 
   const memberships = user?.organizationMemberships;
-  //   memberships?.name
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-8 w-8 rounded-full" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    );
+  }
 
   return (
     <div className="flex cursor-pointer items-center gap-2">
       <Avatar className="h-8 w-8">
         <AvatarImage src={user?.imageUrl} />
-        <AvatarFallback>C</AvatarFallback>
       </Avatar>
-      <p className="font-medium">{user?.fullName} -</p>
+      <p className=" pl-2 font-medium">{user?.fullName} -</p>
       {memberships?.map((m) => m.organization.name)}
     </div>
   );
